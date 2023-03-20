fa506bc1256a563e6d7ec15327705f2f52c5400b
apkawa <apkawa@gmail.com>
2023-03-20 16:59:27 +0300

    test: fix doctest
    Signed-off-by: apkawa <apkawa@gmail.com>

--- src/fixture.rs
+++ src/fixture.rs
@@ -7,10 +7,10 @@ use std::path::{Path, PathBuf};
 /// use test_helpers::fixture::get_git_root;
 /// env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap());
 /// let r = get_git_root().unwrap();
-/// assert!(r.ends_with("rust-any2feed"));
+/// assert!(r.ends_with("test_helpers"));
 /// env::set_current_dir(r.join("test_utils/src/fixtures"));
 /// let r = get_git_root().unwrap();
-/// assert!(r.ends_with("rust-any2feed"));
+/// assert!(r.ends_with("test_helpers"));
 /// ```
 pub fn get_git_root() -> Option<PathBuf> {
     let mut dir = env::current_dir().ok()?;
@@ -32,10 +32,10 @@ pub fn get_git_root() -> Option<PathBuf> {
 /// use test_helpers::fixture::{get_git_root, path_from_git_root};
 /// env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap());
 /// let r = get_git_root().unwrap();
-/// assert!(r.ends_with("rust-any2feed"));
+/// assert!(r.ends_with("test_helpersV"));
 /// env::set_current_dir(r.join("test_utils/src/fixtures"));
 /// let r = path_from_git_root("./foo_bar/").unwrap();
-/// assert!(r.ends_with("rust-any2feed/foo_bar/"));
+/// assert!(r.ends_with("test_helpers/foo_bar/"));
 /// ```
 pub fn path_from_git_root(path: &str) -> Option<PathBuf> {
     Some(get_git_root()?.join(path))
